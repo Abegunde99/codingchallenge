d2789cc49af3d567e3ee029a8b5dadcf8354fc15
Abegunde Precious Olanrewaju <preciousolanrewaju1998@gmail.com>
2024-03-25 11:11:01 +0100

    feat: authentication.js

--- backend-code-challenge/senior/backend/src/user/milestones/authentication.js
+++ backend-code-challenge/senior/backend/src/user/milestones/authentication.js
@@ -1,16 +1,45 @@
 'use strict';
 
-// const {
-//   Errors: { BadRequestError },
-// } = require('finale-rest');
+const {
+  Errors: { BadRequestError },
+} = require('finale-rest');
 
-// const {
-//   API: {
-//     HEADERS: { X_SLUG },
-//     SLUGS: { MYSELF },
-//   },
-// } = require('../../config');
+const {
+  API: {
+    HEADERS: { X_SLUG },
+    SLUGS: { MYSELF },
+  }, API,
+} = require('../../config');
 
-module.exports = (req, res, context) => {
-  return context.continue;
+const { UserModel } = require('../model');
+
+module.exports = async (req, res, context) => {
+  try {
+    // Identify user based on the slug
+    const slug = req.headers[X_SLUG] || req.headers['x-slug'];
+    if (!slug) {
+      throw new BadRequestError('User authentication failed: x-slug header is missing.');
+    }
+
+    // Decode the base64 encoded slug
+    const decodedSlug = Buffer.from(slug, 'base64').toString('utf-8');
+
+    // Get user with slug === decodedSlug
+    const user = await UserModel.findOne({
+      where: { slug: decodedSlug },
+    });
+
+    if (!user) {
+      throw new BadRequestError('user not found.');
+    }
+
+    // Attach user to the request object
+    req.user = user;
+
+    return context.continue;
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    return res.status(500).json({ message: 'Internal server error.' });
+  }
 };
+
